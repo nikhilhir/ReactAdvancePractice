fc1e2c57ae879eb3d9c810500b97d18092e416ce
NIKHIL HIRULKAR <97525857+nikhilhir@users.noreply.github.com>
2022-08-18 23:49:06 +0530

    e1

--- EVALUTION_RCT211/E1/rct-211-b18-e1-boilerplate/src/Components/ProfileDataRow.jsx
+++ EVALUTION_RCT211/E1/rct-211-b18-e1-boilerplate/src/Components/ProfileDataRow.jsx
@@ -5,13 +5,16 @@ const ProfileDataRow = ({ profile }) => {
 
   return (
     <tr data-cy={`profile-wrapper-${profile.id}`}>
-      <td data-cy="profile-id"></td>
-      <td data-cy="profile-image"></td>
-      <td data-cy="profile-first-name"></td>
-      <td data-cy="profile-last-name"></td>
-      <td data-cy="profile-email"></td>
-      <td data-cy="profile-gender"></td>
-      <td data-cy="profile-country"></td>
+      <td data-cy="profile-id">{profile.id}</td>
+      <td data-cy="profile-image">
+        <img style={{borderRadius:"5px", width:"150px"}} src={profile.profile_pic}
+        alt={profile.first_name}/>
+      </td>
+      <td data-cy="profile-first-name">{profile.first_name}</td>
+      <td data-cy="profile-last-name">{profile.last_name}</td>
+      <td data-cy="profile-email">{profile.email}</td>
+      <td data-cy="profile-gender">{profile.gender}</td>
+      <td data-cy="profile-country">{profile.country}</td>
     </tr>
   );
 };
--- EVALUTION_RCT211/E1/rct-211-b18-e1-boilerplate/src/Pages/Homepage.jsx
+++ EVALUTION_RCT211/E1/rct-211-b18-e1-boilerplate/src/Pages/Homepage.jsx
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getProfileDetailrequest, getProfileDetailsFailure, getProfileDetailsSuccess } from "../Redux/action";
+import ProfileDataRow from "../Components/ProfileDataRow";
 
 const Homepage = () => {
+
+const data = useSelector((store) => store.profileData);
+const dispatch=useDispatch()
+
+const getData = ()=>{
+  dispatch(getProfileDetailrequest())
+  axios
+  .get("http://localhost:8080/profile")
+  .then((res)=>dispatch(getProfileDetailsSuccess(res.data)))
+  .catch((err)=>dispatch(getProfileDetailsFailure(err)))
+}
+
+useEffect(()=>{
+  if(data.length===0){
+    getData()
+  }
+},[])
+
+console.log(data)
+
   return (
     <div>
-      <table>
+      <table style={{ margin: "auto" }}>
         <thead style={{ fontWeight: "700" }}>
           <tr>
-            <th>ID</th>
-            <th>Image</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Gender</th>
-            <th>Country</th>
+            <th style={{ width: "150px" }}>ID</th>
+            <th style={{ width: "150px" }}>Image</th>
+            <th style={{ width: "150px" }}>First Name</th>
+            <th style={{ width: "150px" }}> Last Name</th>
+            <th style={{ width: "150px" }}>Email</th>
+            <th style={{ width: "150px" }}>Gender</th>
+            <th style={{ width: "150px" }}>Country</th>
           </tr>
         </thead>
         <tbody data-cy="profile-wrapper">
           {/* Map through the profileData received from the json-server on mounting the component to show it in a table format */}
+          {data?.length > 0 &&
+            data.map((elem) => {
+              return <ProfileDataRow key={elem.id} profile={elem} />;
+              //////////////////////////////////////here pass the profile data as props
+              // return <tr>
+              //   <th>{elem.id}</th>
+              //   <th>{elem.first_name}</th>
+              //   <th><img src="" alt=""/></th>
+              // </tr>;
+            })}
         </tbody>
       </table>
     </div>
