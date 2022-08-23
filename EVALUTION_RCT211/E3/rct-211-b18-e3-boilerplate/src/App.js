d3495d56a82e37d595b91a7b10f0e31ed2bed0e9
NIKHIL HIRULKAR <97525857+nikhilhir@users.noreply.github.com>
2022-08-23 16:33:52 +0530

    commit

--- EVALUTION_RCT211/E3/rct-211-b18-e3-boilerplate/src/App.js
+++ EVALUTION_RCT211/E3/rct-211-b18-e3-boilerplate/src/App.js
@@ -9,7 +9,7 @@ function App() {
   2. Create the custom components, inside the respective files of Image.jsx and Image.jsx
   3. The application should function properly after the implimentation of hooks and Custom components
   */
-  const { countValue, incCount, decCount } = useCounter({
+  const { count, incCount, decCount } = useCounter({
     initialValue: 10,
     minValue: 5,
     maxValue: 15,
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       {/* DO NOT CHANGE the 5 lines below   */}
-      <h3>Value: {countValue}</h3>
+      <h3>Value: {count}</h3>
       <button onClick={() => incCount()}>Increment</button>
       <button onClick={() => incCount(3)}>Increment 3</button>
       <button onClick={() => decCount(2)}>Decrement 2</button>
--- EVALUTION_RCT211/E3/rct-211-b18-e3-boilerplate/src/hooks/useCounter.js
+++ EVALUTION_RCT211/E3/rct-211-b18-e3-boilerplate/src/hooks/useCounter.js
@@ -3,13 +3,96 @@
 2. Feel free to change any boilerplate logic present inside this custom hook
 3. DO NOT CHANGE the name `useCounter` of this custom hook
 */
-export const useCounter = () => {
-  const countValue = "";
-  const incCount = () => {};
-  const decCount = () => {};
-  return {
-    countValue,
-    incCount,
-    decCount,
-  };
+// export const useCounter = () => {
+//   const countValue = "";
+//   const incCount = () => {};
+//   const decCount = () => {};
+//   return {
+//     countValue,
+//     incCount,
+//     decCount,
+//   };
+// };
+// **********************************given**************
+
+import { useCallback, useState } from "react";
+
+// import { useState } from "react";
+
+// /* 
+// 1. Complete the useCounter hook functionality here
+// 2. Feel free to change any boilerplate logic present inside this custom hook
+// 3. DO NOT CHANGE the name `useCounter` of this custom hook
+// */
+// export const useCounter = (init) => {
+//   const [countValue, setCount] = useState(init.initialValue);
+//   const incCount = (value) => {
+//     if (countValue < init.maxValue) {
+//       if (value == null) {
+//         setCount(countValue + 1);
+//       } else {
+//         setCount(countValue + value);
+//       }
+//     }
+//   };
+//   const decCount = (value) => {
+//     if (countValue > init.minValue) {
+//       if (value == null) {
+//         setCount(countValue - 1);
+//       } else {
+//         setCount(countValue - value);
+//       }
+//     }
+//   };
+//   return {
+//     countValue,
+//     incCount,
+//     decCount,
+//   };
+// };
+// ****************************************CUSTOM HOOK USECOUNTER*************************
+
+export const useCounter =({initialCount,minvalue,maxValue})=>{
+
+const [count,setCount]=useState(initialCount)
+
+const onIncrement = useCallback(
+  (incrementBy = 1) => {
+    setCount((count) => {
+      if (count >= maxValue) {
+        return count;
+      }
+      if (maxValue === 0 && count === 0) {
+        return 0;
+      }
+      return maxValue && count &&   incrementBy+count > maxValue
+        ? count
+        : count + incrementBy;
+    });
+  },
+  [ maxValue]
+);
+
+
+
+const onDecrement = useCallback(
+  (decrementBy =1)=>{
+    setCount((count)=>{
+      if(count <= minvalue){
+        return count;
+      }
+      if(count===0 && minvalue===0){
+        return 0;
+      }
+      return minvalue && count && count-decrementBy < minvalue ? count : count - decrementBy;
+    })
+  },
+  [setCount,minvalue]
+)
+
+return {
+  count,
+  incCount: onIncrement,
+  decCount: onDecrement,
 };
+}
