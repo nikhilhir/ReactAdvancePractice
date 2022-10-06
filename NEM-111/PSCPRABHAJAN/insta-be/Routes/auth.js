0dfd361537a1c685065baffb0dd46a93f77b20ae
NIKHIL HIRULKAR <97525857+nikhilhir@users.noreply.github.com>
2022-10-06 23:32:11 +0530

    mern

---
+++ NEM-111/PSCPRABHAJAN/insta-be/Routes/auth.js
@@ -0,0 +1,14 @@
+
+
+//const express = require("express");
+//const app=express()
+
+const {Router} = require("express")
+const authRouter =Router();
+ //const authrouter = express.Router()
+
+authRouter.post("/signup",(req,res)=>{})
+
+authRouter.post("/login",(req,res)=>{})
+
+module.exports = authRouter;
--- NEM-111/PSCPRABHAJAN/insta-be/index.js
+++ NEM-111/PSCPRABHAJAN/insta-be/index.js
@@ -1,16 +1,24 @@
 
 const express = require("express");
+ const authRouter = require("./Routes/auth");
 
  const app=express()
+
+ app.use(express.urlencoded({extended:true}));
+ app.use=(express.json());
+
+
+
 
  
 
 
 
- app.get("/",(req,res)=>{
-
-    res.send("hello world")
+app.get("/",(req,res)=>{
+res.send("hello world start node")
  })
+
+//app.use("/auth",authRouter)
 
 
  app.listen(5000,()=>{
