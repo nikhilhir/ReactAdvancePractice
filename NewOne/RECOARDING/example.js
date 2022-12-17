ec518482921df878ca3b8bac6c7ab424f7eeee73
NIKHIL HIRULKAR <97525857+nikhilhir@users.noreply.github.com>
2022-12-17 23:41:33 +0530

    commit

---
+++ NewOne/RECOARDING/example.js
@@ -0,0 +1,78 @@
+
+// we wiil see
+// Local vs Global scope
+// var vs let vs const with example
+// different types of functions in JavaScript
+// Function expression
+// Function Statement
+// Anonymous function
+// Named Function Expression
+
+// const { DiffieHellmanGroup } = require("crypto");
+
+
+
+// console.log("hello world")
+
+ 
+// {
+//     var xyz="hello"
+   
+// }
+//  console.log(xyz);
+
+//  //difference be VAR LET CONST
+
+//  var x="hello world"    
+//  var x="revision"
+//  x="javascript"
+
+
+
+// //  **************************
+
+//  let  x="hello world"    
+//  let  x="revision"
+//  x="javascript"
+
+
+// //  ******************************************
+
+// const x = "hello world";
+
+// x = "javascript";
+
+
+// Function expression
+// Function Statement
+// Anonymous function
+// Named Function Expression
+
+
+// var addition = function add(){
+
+//     console.log("hello world")
+// }
+// addition();
+
+
+ //Function Statement
+// xyz();
+//  function xyz(){
+//     console.log("revision")
+//  }
+
+ // Anonymous function
+
+//  function(){
+
+//  }
+
+
+ // Named Function Expression
+
+  var x= function xyz(){
+    console.log("revision")
+  }
+
+  x()
